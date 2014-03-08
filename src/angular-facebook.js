e06a95e915f76f9dd3e6b96d09bddb894b5ffb6d
Joon Ho Cho <joon@42go.com>
2014-03-07 16:53:57 -0800

    initial implementation

--- src/angular-facebook.js
+++ src/angular-facebook.js
@@ -1,158 +1,196 @@
+/* global angular */
 angular.module('jun.facebook', [])
 
-.provider('$FB', function () {
-	'use strict';
+.provider('$FB', [
+	'$window', '$q',
+	function $FBProvider($window, $q) {
+		'use strict';
 
-	var that = this,
-		options = angular.extend({}, {
-			// Default option values
+		var that = this,
+			options = {
+				// Default option values
 
-			// FB.init params
-			// https://developers.facebook.com/docs/javascript/reference/FB.init/
-			appId: null,
-			cookie: false,
-			logging: true,
-			status: true,
-			xfbml: false,
-			authResponse: void 0,
-			frictionlessRequests: false,
-			hideFlashCallback: null,
+				// FB.init params
+				// https://developers.facebook.com/docs/javascript/reference/FB.init/
+				appId: null,
+				cookie: false,
+				logging: true,
+				status: true,
+				xfbml: false,
+				authResponse: void 0,
+				frictionlessRequests: false,
+				hideFlashCallback: null,
 
-			// more options
-			permissions: ''
-		});
+				// FB.login options
+				// https://developers.facebook.com/docs/reference/javascript/FB.login
+				scope: '',
+				'enable_profile_selector': false,
+				'profile_selector_ids': ''
+			};
 
-	function getSetOption(name, val) {
-		if (val === void 0) {
-			return options[name];
+		function getSetOption(name, val) {
+			if (val === void 0) {
+				return options[name];
+			}
+			options[name] = val;
+			return that;
 		}
-		options[name] = val;
-		return this;
-	}
 
-	angular.forEach(['appId', 'cookie', 'logging', 'status', 'xfbml', 'authResponse', 'frictionlessRequests', 'hideFlashCallback', 'permissions'], function (name) {
-		this[name] = angular.bind(getSetOption, this, name);
-	}, this);
+		angular.forEach([
+			'appId',
+			'cookie',
+			'logging',
+			'status',
+			'xfbml',
+			'authResponse',
+			'frictionlessRequests',
+			'hideFlashCallback',
+			'permissions'
+		], function (name) {
+			this[name] = angular.bind(getSetOption, this, name);
+		}, this);
 
-	this.option = function (name, val) {
-		if (typeof name === 'object') {
-			angular.extend(options, name);
-			return this;
-		}
-		return getSetOption.call(this, name, val);
-	};
+		this.option = function (name, val) {
+			if (typeof name === 'object') {
+				angular.extend(options, name);
+				return this;
+			}
+			return getSetOption(name, val);
+		};
 
-	this.$get = [
-		'$q', '$timeout',
-		function ($q, $timeout) {
-			var deferred = $q.defer();
+		var FBPromise;
 
-			if (options.appId) {
-				// https://developers.facebook.com/docs/javascript/quickstart
-				window.fbAsyncInit = function () {
-					FB.init({
-						appId: options.appId,
-						cookie: options.cookie,
-						logging: options.logging,
-						status: options.status,
-						xfbml: options.xfbml,
-						authResponse: options.authResponse,
-						frictionlessRequests: options.frictionlessRequests,
-						hideFlashCallback: options.hideFlashCallback
-					});
-					$timeout(function () {
-						deferred.resolve(FB);
-					});
-				};
+		this.$get = [
+			'$timeout',
+			function ($timeout) {
+				var deferred = $q.defer();
 
-				(function (d, s, id) {
-					var js, fjs = d.getElementsByTagName(s)[0];
-					if (d.getElementById(id)) {
-						return;
+				if (options.appId) {
+					// https://developers.facebook.com/docs/javascript/quickstart
+					var window = $window,
+						document = window.document;
+
+					window.fbAsyncInit = function () {
+						var FB = window.FB;
+						FB.init({
+							appId: options.appId,
+							cookie: options.cookie,
+							logging: options.logging,
+							status: options.status,
+							xfbml: options.xfbml,
+							authResponse: options.authResponse,
+							frictionlessRequests: options.frictionlessRequests,
+							hideFlashCallback: options.hideFlashCallback
+						});
+						$timeout(function () {
+							deferred.resolve(FB);
+						});
+					};
+
+					(function (d, s, id) {
+						var js, fjs = d.getElementsByTagName(s)[0];
+						if (d.getElementById(id)) {
+							return;
+						}
+						js = d.createElement(s);
+						js.id = id;
+						js.src = '//connect.facebook.net/en_US/all.js';
+						fjs.parentNode.insertBefore(js, fjs);
+					}(document, 'script', 'facebook-jssdk'));
+				}
+				else {
+					deferred.reject('$FB: appId is not set!');
+				}
+
+				FBPromise = deferred.promise;
+				return FBPromise;
+			}
+		];
+
+		this.getLoginStatus = function () {
+			// https://developers.facebook.com/docs/reference/javascript/FB.getLoginStatus
+			return FBPromise.then(function (FB) {
+				var deferred = $q.defer();
+				FB.getLoginStatus(angular.bind(deferred.resolve, deferred));
+				return deferred.promise;
+			});
+		};
+
+		this.api = function () {
+			var apiArgs = arguments;
+
+			// https://developers.facebook.com/docs/javascript/reference/FB.api
+			return FBPromise.then(function (FB) {
+				var deferred = $q.defer(),
+					args = Array.prototype.slice.call(apiArgs),
+					callback;
+
+				if (typeof args[args.length - 1] === 'function') {
+					callback = args.pop();
+				}
+
+				args.push(function (response) {
+					if (!response || response.error) {
+						deferred.reject(response || {
+							error: true
+						});
 					}
-					js = d.createElement(s);
-					js.id = id;
-					js.src = "//connect.facebook.net/en_US/all.js";
-					fjs.parentNode.insertBefore(js, fjs);
-				}(document, 'script', 'facebook-jssdk'));
-			}
-			else {
-				deferred.reject('$FB: appId is not set!');
-			}
+					else {
+						deferred.resolve(response);
+					}
+				});
 
-			return deferred.promise;
-		}
-	];
-});
-/*
-    angular.module("facebookUtils").service("facebookUser", [ "$window", "$rootScope", "$q", "facebookConfigDefaults", "facebookConfigSettings", "facebookSDK", function($window, $rootScope, $q, facebookConfigDefaults, facebookConfigSettings, facebookSDK) {
-        var FacebookUser = function() {};
-        var checkStatus = function() {
-            var deferred = $q.defer();
-            FB.getLoginStatus(function(response) {
-                $rootScope.$apply(function() {
-                    if (response.status === "connected") {
-                        user.loggedIn = true;
-                        $rootScope.$broadcast("fbLoginSuccess", response);
-                        deferred.resolve(response);
-                    } else {
-                        user.loggedIn = false;
-                        deferred.reject(response);
-                    }
-                });
-            }, true);
-            return deferred.promise;
-        };
-        FacebookUser.prototype.loggedIn = false;
-        FacebookUser.prototype.api = function() {
-            var deferred = $q.defer();
-            var args = [].splice.call(arguments, 0);
-            args.push(function(response) {
-                $rootScope.$apply(function() {
-                    deferred.resolve(response);
-                });
-            });
-            FB.api.apply(FB, args);
-            return deferred.promise;
-        };
-        FacebookUser.prototype.login = function() {
-            var _self = this;
-            FB.login(function(response) {
-                if (response.authResponse) {
-                    response.userNotAuthorized = true;
-                    _self.loggedIn = true;
-                    $rootScope.$broadcast("fbLoginSuccess", response);
-                } else {
-                    _self.loggedIn = false;
-                    $rootScope.$broadcast("fbLoginFailure");
-                }
-            }, {
-                scope: facebookConfigSettings.permissions || facebookConfigDefaults.permissions
-            });
-        };
-        FacebookUser.prototype.logout = function() {
-            var _self = this;
-            FB.logout(function(response) {
-                if (response) {
-                    _self.loggedIn = false;
-                    $rootScope.$broadcast("fbLogoutSuccess");
-                } else {
-                    $rootScope.$broadcast("fbLogoutFailure");
-                }
-            });
-        };
-        var user = new FacebookUser();
-        var deferred = $q.defer();
-        facebookSDK.then(function() {
-            checkStatus()["finally"](function() {
-                deferred.resolve(user);
-            });
-        }, function() {
-            deferred.reject("SDK failed to load because your app ID wasn't set");
-        });
-        return deferred.promise;
-    } ]);
-})({}, function() {
-    return this;
-}());
-*/
+				FB.api.apply(FB, args);
+
+				if (callback) {
+					return deferred.promise.then(callback);
+				}
+				return deferred.promise;
+			});
+		};
+
+		this.login = function (callback, opts) {
+			// https://developers.facebook.com/docs/reference/javascript/FB.login
+			return FBPromise.then(function (FB) {
+				var deferred = $q.defer();
+
+				if (typeof callback !== 'function') {
+					callback = null;
+					opts = callback;
+				}
+
+				function getOpt(name) {
+					var val = opts && opts[name];
+					return val === void 0 ? options[name] : val;
+				}
+
+				FB.login(function (response) {
+					if (response.authResponse) {
+						deferred.resolve(response);
+					}
+					else {
+						deferred.reject(response);
+					}
+				}, {
+					scope: getOpt('scope'),
+					'enable_profile_selector': getOpt('enable_profile_selector'),
+					'profile_selector_ids': getOpt('profile_selector_ids')
+				});
+
+				if (callback) {
+					return deferred.promise.then(callback);
+				}
+				return deferred.promise;
+			});
+		};
+
+		this.logout = function () {
+			// https://developers.facebook.com/docs/reference/javascript/FB.logout
+			return FBPromise.then(function (FB) {
+				var deferred = $q.defer();
+				FB.logout(angular.bind(deferred.resolve, deferred));
+				return deferred.promise;
+			});
+		};
+	}
+]);
