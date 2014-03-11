02dc606dc7082e33c198261f7f6ec6c42e52b44d
Joon Ho Cho <joon@42go.com>
2014-03-11 16:14:13 -0700

    build

--- dist/angular-facebook-api.js
+++ dist/angular-facebook-api.js
@@ -1,4 +1,4 @@
-/*! angular-facebook-api - v0.0.17 - 2014-03-11 */
+/*! angular-facebook-api - v0.0.18 - 2014-03-11 */
 /* global angular */
 angular.module('jun.facebook', [])
 
@@ -63,7 +63,7 @@ angular.module('jun.facebook', [])
 		return getSetOption(name, val);
 	};
 
-	var FB, FBPromise, $q;
+	var FB, FBPromise, initPromise, $window, $timeout, $q;
 
 	/*
 	 * Initialization
@@ -71,48 +71,10 @@ angular.module('jun.facebook', [])
 
 	this.$get = [
 		'$window', '$timeout', '$q',
-		function ($window, $timeout, $$q) {
+		function ($$window, $$timeout, $$q) {
 			$q = $$q;
-			var deferred = $q.defer();
-
-			if (options.appId) {
-				// https://developers.facebook.com/docs/javascript/quickstart
-				var window = $window,
-					document = window.document;
-
-				window.fbAsyncInit = function () {
-					FB = that.FB = window.FB;
-					FB.init({
-						appId: options.appId,
-						cookie: options.cookie,
-						logging: options.logging,
-						status: options.status,
-						xfbml: options.xfbml,
-						authResponse: options.authResponse,
-						frictionlessRequests: options.frictionlessRequests,
-						hideFlashCallback: options.hideFlashCallback
-					});
-					$timeout(function () {
-						deferred.resolve(FB);
-					});
-				};
-
-				(function (d, s, id) {
-					var js, fjs = d.getElementsByTagName(s)[0];
-					if (d.getElementById(id)) {
-						return;
-					}
-					js = d.createElement(s);
-					js.id = id;
-					js.src = '//connect.facebook.net/en_US/all.js';
-					fjs.parentNode.insertBefore(js, fjs);
-				}(document, 'script', 'facebook-jssdk'));
-			}
-			else {
-				deferred.reject('$FB: appId is not set!');
-			}
-
-			FBPromise = deferred.promise;
+			$window = $$window;
+			$timeout = $$timeout;
 			return that;
 		}
 	];
@@ -142,9 +104,64 @@ angular.module('jun.facebook', [])
 	/*
 	 * Public APIs
 	 */
+
+	this.load = function () {
+		if (!FBPromise) {
+			var window = $window,
+				deferred = $q.defer();
+
+			// https://developers.facebook.com/docs/javascript/quickstart
+			window.fbAsyncInit = function () {
+				FB = that.FB = window.FB;
+				$timeout(function () {
+					deferred.resolve(FB);
+				});
+			};
+
+			(function (d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) {
+					return;
+				}
+				js = d.createElement(s);
+				js.id = id;
+				js.src = '//connect.facebook.net/en_US/all.js';
+				fjs.parentNode.insertBefore(js, fjs);
+			}(window.document, 'script', 'facebook-jssdk'));
+
+			FBPromise = deferred.promise;
+		}
+		return FBPromise;
+	};
+
+	this.init = function (params) {
+		if (!initPromise) {
+			initPromise = that.load().then(function (FB) {
+				params = angular.extend({
+					appId: options.appId,
+					cookie: options.cookie,
+					logging: options.logging,
+					status: options.status,
+					xfbml: options.xfbml,
+					authResponse: options.authResponse,
+					frictionlessRequests: options.frictionlessRequests,
+					hideFlashCallback: options.hideFlashCallback
+				}, params);
+
+				if (!params.appId) {
+					throw new Error('$FB: appId is not set!');
+				}
+
+				FB.init(params);
+				return FB;
+			});
+		}
+		return initPromise;
+	};
+
 	this.getLoginStatus = function (callback) {
 		// https://developers.facebook.com/docs/reference/javascript/FB.getLoginStatus
-		return FBPromise.then(function (FB) {
+		return that.init().then(function (FB) {
 			var deferred = $q.defer();
 
 			FB.getLoginStatus(angular.bind(deferred, handleResponse));
@@ -157,7 +174,7 @@ angular.module('jun.facebook', [])
 		var apiArgs = arguments;
 
 		// https://developers.facebook.com/docs/javascript/reference/FB.api
-		return FBPromise.then(function (FB) {
+		return that.init().then(function (FB) {
 			var deferred = $q.defer(),
 				args = Array.prototype.slice.call(apiArgs),
 				callback;
@@ -176,7 +193,7 @@ angular.module('jun.facebook', [])
 
 	this.login = function (callback, opts) {
 		// https://developers.facebook.com/docs/reference/javascript/FB.login
-		return FBPromise.then(function (FB) {
+		return that.init().then(function (FB) {
 			var deferred = $q.defer();
 
 			if (typeof callback !== 'function') {
@@ -218,7 +235,7 @@ angular.module('jun.facebook', [])
 
 	this.disconnect = function (callback) {
 		// http://stackoverflow.com/questions/6634212/remove-the-application-from-a-user-using-graph-api/7741978#7741978
-		return FBPromise.then(function (FB) {
+		return that.init().then(function (FB) {
 			var deferred = $q.defer();
 
 			FB.api('/me/permissions', 'DELETE', angular.bind(deferred, handleResponse));
